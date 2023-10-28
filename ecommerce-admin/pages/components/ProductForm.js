d13fd6da91570acbada9f76985039a591bb68b96
DaoTruongNghi <daotruongnghi0812@gmail.com>
2023-10-28 21:43:34 +0700

    Ready to build api/upload.js

--- ecommerce-admin/pages/components/ProductForm.js
+++ ecommerce-admin/pages/components/ProductForm.js
@@ -9,6 +9,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   // State
   const [title, setTitle] = useState(existingTitle || "");
@@ -35,6 +36,16 @@ export default function ProductForm({
 
   if (goToProducts) router.push("/products");
 
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      console.log(files);
+      files.forEach((file) => data.append("file", file));
+      const res = await axios.post("/api/upload", data);
+      console.log(res.data);
+    }
+  }
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -44,6 +55,32 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label
+          className="w-24 h-24 flex flex-col justify-center 
+          items-center rounded-lg text-sm text-gray-600 gap-1
+          bg-gray-200 cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden"></input>
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
 
       <label>Description</label>
       <textarea
